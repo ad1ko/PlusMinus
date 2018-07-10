a6bfa4a7d4d3857b760596188d6a4d80b1300076
Aizada <aizadaboposhova@gmail.com>
2018-07-10 16:56:53 -0500

    Adding some changes, Aizada

--- PlusMinus.java
+++ PlusMinus.java
@@ -25,7 +25,7 @@ public class PlusMinus {
 	public static void main(String[] args) {
 		int arr[] = {-4, 3, -9, 0, 4, 1 };
 		plusMinus(arr);
-		
+		System.out.println("Message from Aizada");
 
 
 	}
