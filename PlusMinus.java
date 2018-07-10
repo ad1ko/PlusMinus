081cc5698a86508de063f11f210aebef9f308027
Adilet <adikolanov@gmail.com>
2018-07-10 16:22:26 -0500

    manty lovers

---
+++ PlusMinus.java
@@ -0,0 +1,33 @@
+package may09;
+
+public class PlusMinus {
+	static void plusMinus(int[] arr) {
+        double pos=0;
+        double neg = 0;
+        double zero = 0;
+        int len=arr.length; 
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i]>0){
+                pos++;
+            }else if(arr[i]<0){
+                neg++;
+            }else{
+                zero++;
+            }
+    }
+        System.out.println(pos/len);
+        System.out.println(neg/len);
+        System.out.println(zero/len);
+        
+
+    }
+
+	public static void main(String[] args) {
+		int arr[] = {-4, 3, -9, 0, 4, 1 };
+		plusMinus(arr);
+		
+
+
+	}
+
+}
