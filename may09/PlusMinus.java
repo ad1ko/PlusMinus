5a8b5ae31d490636bd481dfa7247a5a455bbb1b2
Huseyin Altas <altas2001@hotmail.com>
2018-07-11 11:38:56 -0500

    Huseyin made some changes

---
+++ may09/PlusMinus.java
@@ -0,0 +1,38 @@
+package may09;
+//hi Adilet this is from Nargiza
+public class PlusMinus {
+	static void plusMinus(int[] arr) {
+        double pos=0;
+        double neg = 0;
+        double zero = 0;
+        int len=arr.length; 
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i]>0){
+                pos++;
+            }else if(arr[i]<0){
+                neg++;
+            }else{
+                zero++;
+            }
+    }
+        System.out.println(pos/len);
+        System.out.println(neg/len);
+        System.out.println(zero/len);
+        System.out.println("Nargiza");
+
+    }
+
+	public static void main(String[] args) {
+		int arr[] = {-4, 3, -9, 0, 4, 1 };
+		plusMinus(arr);
+		
+		System.out.println("adilet kaleysan");
+		
+
+System.out.println("akmal has made some changes");
+
+//HUSEYIN WAS HERE
+	}
+	
+
+}
